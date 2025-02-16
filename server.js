93e54415f6d593219f30b7e734802c88273fbfe2
JaRose <newson.jaluan16@gmail.com>
2025-02-16 13:07:05 -0600

    added conection

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ const port = 3000;
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: test,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("hello!!!");
